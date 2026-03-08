86851ec36d8faeacaa6f6560212d47c1ed6ab655
drodo2 <noreply@rgsstoregamming.com>
2026-03-08 17:10:36 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -25,7 +25,7 @@ echo ------------------------------------------
 
 tasklist | find /i "sing-box.exe" >nul
 if %errorlevel% equ 0 (
-    echo [OK] sing-box VPN Active.
+    echo [OK] sing-box VPN Active - SOCKS5 127.0.0.1:2080
 ) else (
     echo [WARN] sing-box mati, restart...
     powershell -Command "Start-Process -FilePath 'D:\a\RDP-Free-6-Hours\RDP-Free-6-Hours\sing-box.exe' -ArgumentList 'run','-c','D:\a\RDP-Free-6-Hours\RDP-Free-6-Hours\config.json' -WindowStyle Hidden"
@@ -35,5 +35,5 @@ echo ------------------------------------------
 echo Loop setiap 5 menit. Cancel workflow untuk stop.
 echo ------------------------------------------
 
-timeout /t 300 /nobreak >nul
+timeout /t 300
 goto check
