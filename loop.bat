8a6f7d8524e00cda4ed338c5577a1a95bb7108da
drodo2 <noreply@rgsstoregamming.com>
2026-03-08 17:06:12 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -12,12 +12,12 @@ echo ==========================================
 
 tasklist | find /i "tailscaled.exe" >nul
 if %errorlevel% equ 0 (
-    echo [OK] Tailscale Service Active.
+    echo [OK] Tailscale Active.
     echo [INFO] Hostname: RDP-Ery-Bogor
     echo [IP ADDRESS]:
     "C:\Program Files\Tailscale\tailscale.exe" ip -4
 ) else (
-    echo [ERROR] Tailscale disconnected! Exiting...
+    echo [ERROR] Tailscale disconnected!
     exit /b 1
 )
 
@@ -28,12 +28,11 @@ if %errorlevel% equ 0 (
     echo [OK] sing-box VPN Active.
 ) else (
     echo [WARN] sing-box mati, restart...
-    powershell -Command "Start-ScheduledTask -TaskName 'singbox'"
+    powershell -Command "Start-Process -FilePath 'D:\a\RDP-Free-6-Hours\RDP-Free-6-Hours\sing-box.exe' -ArgumentList 'run','-c','D:\a\RDP-Free-6-Hours\RDP-Free-6-Hours\config.json' -WindowStyle Hidden"
 )
 
 echo ------------------------------------------
-echo Session akan tetap hidup. Loop setiap 5 menit.
-echo Untuk stop, cancel GitHub Workflow.
+echo Loop setiap 5 menit. Cancel workflow untuk stop.
 echo ------------------------------------------
 
 timeout /t 300 /nobreak >nul
