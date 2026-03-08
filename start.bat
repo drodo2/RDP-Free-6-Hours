bd8eec6e6e00a20fc3ffeea9b5f8afab60a1ed71
drodo2 <noreply@rgsstoregamming.com>
2026-03-08 14:47:46 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,12 @@
 @echo off
 echo 🛠️ Setting up User Accounts...
-:: Mengambil password dari GitHub Secret (RDP_PASSWORD)
 net user administrator %RDP_PASSWORD% /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
+
+echo 🌐 Optimizing Network & DNS...
+:: Mengganti DNS ke Google agar akses ke rgsstoregamming.com lancar
+powershell -Command "Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses ('8.8.8.8','8.8.4.4')"
 
 echo ⏬ Downloading Tailscale...
 curl -L https://pkgs.tailscale.com/stable/tailscale-setup-latest.exe -o tailscale-setup.exe >nul
@@ -12,7 +15,6 @@ echo 🚀 Installing Tailscale (Please wait...)
 start /wait tailscale-setup.exe /quiet
 
 echo 🔗 Connecting to Tailscale Mesh Network...
-:: Menggunakan Auth Key dari GitHub Secret (TS_AUTHKEY)
 "C:\Program Files\Tailscale\tailscale.exe" up --authkey=%TS_AUTHKEY% --hostname=RDP-Ery-Bogor --accept-routes
 
 echo ----------------------------------
